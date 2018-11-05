62a2960e059ab7deabd0eb16833c4bbc04d5fd87
weekdawn <571426779@qq.com>
2018-11-05 15:25:10 +0800

    add two folders

---
+++ second_homework/udp_client.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define PORT 15001
+#define MAXDATA 100
+
+int main(int argc, char* argv[])
+{
+	int sockfd, sendtofd, recvfromfd;
+	char buf[MAXDATA];
+	struct hostent* he;
+	struct sockaddr_in server_addr, reply_addr;
+	
+	if(argc != 2)
+	{
+		printf("Usage: %s <IP address>\n", argv[0]);
+		exit(-1);
+	}
+	
+	//命令行接收服务器的IP地址
+	if((he = gethostbyname(argv[1])) == NULL)
+	{
+		perror("Gethostbyname error.");
+		exit(-1);
+	}else
+	{
+		printf("Gethostbyname...\n");
+	}
+	
+	//创建socket
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sockfd == -1)
+	{
+		perror("Create socket failed.");
+		exit(-1);
+	}else
+	{
+		printf("Create socket...\n");
+	}
+	
+	bzero(&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr = *((struct in_addr*)he -> h_addr);
+	
+	
+	while(1)
+	{
+		int len;
+		printf("Please type your msg ：");
+		//scanf("%s", &buf);
+		gets(buf);
+		sendtofd = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr*)&server_addr, sizeof(struct sockaddr));
+		
+		if(!strcmp(buf, "exit"))
+			break;
+		
+		if(sendtofd == -1)
+		{
+			perror("Sendto error.");
+			exit(-1);
+		}
+		recvfromfd = recvfrom(sockfd, buf, MAXDATA, 0, (struct sockaddr*)&reply_addr, &len);
+		if(recvfromfd == -1)
+		{
+			perror("Recvfrom error.");
+			exit(-1);
+		}
+		
+		if(len != sizeof(struct sockaddr) || memcmp ((const void *)&server_addr, (const void*)&reply_addr, len) != 0)
+		{
+			printf("Receive message from other server.\n");
+			continue;
+		}
+
+		buf[MAXDATA] = '\0';
+		printf("Received message: %s\n", buf);
+
+	}
+	
+	close(sockfd);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ second_homework/udp_server.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 15001
+#define MAXDATA 100
+
+
+
+int main(void)
+{
+	int sockfd;
+	struct sockaddr_in server_addr, client_addr;
+	int sin_size, num;
+	char msg[MAXDATA];
+	
+	//建立socket
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sockfd == -1)
+	{
+		perror("Create socket failed.");
+		exit(-1);
+	}else
+	{
+		printf("Create socket successful!\n");
+	}
+	
+	bzero(&server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	//绑定套接字到特定地址
+	if(bind(sockfd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr)) == -1)
+	{
+		perror("Bind error.");
+		exit(-1);
+	}else
+	{
+		printf("Bind successful!\n");
+	}
+	
+	sin_size = sizeof(struct sockaddr_in);
+	while(1)
+	{
+		num = recvfrom(sockfd, msg, MAXDATA, 0, (struct sockaddr *)&client_addr, &sin_size);
+		if(num < 0)
+		{
+			perror("recvfrom error.");
+			exit(-1);
+		}
+		
+		msg[num] = '\0';
+		
+
+		printf("You have been connected from %s : %d \tmsg : %s\n", 
+			    inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port),msg);
+				
+		if(!strcmp(msg, "exit"))
+			break;
+		
+		sendto(sockfd, msg, sizeof(msg), 0, (struct sockaddr*)&client_addr, sin_size);
+		
+	}
+	
+	//关闭连接
+	close(sockfd);
+
+
+
+}
